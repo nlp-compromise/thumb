61128a2b0f422fc69cbe026fbe9a7ae424aae9cf
spencermountain <spencermountain@gmail.com>
2017-05-05 19:59:32 -0400

    rules/exceptions

--- de-data/learn.js
+++ de-data/learn.js
@@ -1,6 +1,21 @@
 var nlpThumb = require('../src');
 var getWords = require('./getWords');
 
+const fmtRules = function(obj) {
+  let rules = obj.rules.reduce((arr, o) => {
+    let num = o.suffix.length;
+    arr[num] = arr[num] || {};
+    arr[num][o.suffix] = 1; //o.exceptions;
+    return arr;
+  }, []);
+  for (let i = 0; i < rules.length; i++) {
+    if (!rules[i]) {
+      rules[i] = {};
+    }
+  }
+  return rules;
+};
+
 //adjectives
 let adj = getWords('JJ');
 adj = adj.filter((a) => a[1] > 1);
@@ -17,5 +32,12 @@ verb = verb.map((a) => a[0]);
 // let rest = verb.concat(nouns);
 
 //inlist, outlist
-var rules = nlpThumb(adj, verb, {});
-console.log(JSON.stringify(rules, null, 2));
+var rules = nlpThumb(verb, adj, {});
+// rules = fmtRules(rules);
+
+let exceptions = [];
+rules.rules.map((o) => {
+  exceptions = exceptions.concat(o.exceptions);
+});
+
+console.log(JSON.stringify(exceptions, null, 2));
