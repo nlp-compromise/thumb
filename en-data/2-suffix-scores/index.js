6b7ac7501024ccf6455488e47c673b08c3bd1186
spencermountain <spencermountain@gmail.com>
2017-05-14 14:34:55 -0400

    score suffixes starting

--- en-data/2-suffix-scores/index.js
+++ en-data/2-suffix-scores/index.js
@@ -11,27 +11,28 @@ for (let len = 4; len >= 2; len -= 1) {
     if (/[ 0-9]/.test(suff) === true) {
       return
     }
-    suffixes[suff] = suffixes[suff] || 0
-    suffixes[suff] += 1
+    suffixes[suff] = suffixes[suff] || {}
+  // suffixes[suff] += 1
   })
 }
-let arr = []
-Object.keys(suffixes).forEach((k) => {
-  if (suffixes[k] > 1) {
-    arr.push({
-      str: k,
-      count: suffixes[k],
-      size: k.length
-    })
-  }
-})
-arr = arr.sort((a, b) => {
-  if (a.size < b.size) {
-    return 1
-  }
-  if (a.count < b.count) {
-    return 1
-  }
-  return -1
-})
-console.log(arr)
+
+// let arr = []
+// Object.keys(suffixes).forEach((k) => {
+//   if (suffixes[k] > 1) {
+//     arr.push({
+//       str: k,
+//       count: suffixes[k],
+//       size: k.length
+//     })
+//   }
+// })
+// arr = arr.sort((a, b) => {
+//   if (a.size < b.size) {
+//     return 1
+//   }
+//   if (a.count < b.count) {
+//     return 1
+//   }
+//   return -1
+// })
+// console.log(arr)
